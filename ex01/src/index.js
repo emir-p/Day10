159ad3a36e3864b70869a8a641c2f47746042c95
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-17 22:19:31 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Merjem",
+    name: "Namik",
+    legs: 7,
+    friends: ["Emir", "Kexi"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
